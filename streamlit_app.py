ab904aa6789e6e460151032e2cf8b9291c6f521c
vishnupriya91 <110082133+vishnupriya91@users.noreply.github.com>
2023-10-12 10:45:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,4 @@ streamlit.write('Thanks for adding ', add_my_fruit)
 
 #This will not cork correctly, but just go with it for now
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
